06cae89e8121dd258d1fd47f9325e75d5c2893bc
Wladimir Wendland <wtw24.com@gmail.com>
2025-05-17 19:11:14 +0200

    refactoring create-bash_history.sh

--- docker/bin/create-bash_history.sh
+++ docker/bin/create-bash_history.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 NODE_BASH_HISTORY_FILE="docker/development/node/bash/.bash_history"
 PHP_CLI_BASH_HISTORY_FILE="docker/development/php-cli/bash/.bash_history"
 
 if [ ! -f "${NODE_BASH_HISTORY_FILE}" ]; then
-    touch ${NODE_BASH_HISTORY_FILE}
+    touch "${NODE_BASH_HISTORY_FILE}"
+    echo "Created ${NODE_BASH_HISTORY_FILE}"
 fi
 
 if [ ! -f "${PHP_CLI_BASH_HISTORY_FILE}" ]; then
-    touch ${PHP_CLI_BASH_HISTORY_FILE}
+    touch "${PHP_CLI_BASH_HISTORY_FILE}"
+    echo "Created ${PHP_CLI_BASH_HISTORY_FILE}"
 fi
