91b6b6565635f2d48165dee8a62c45108104bbc9
Wladimir Wendland <wtw24.com@gmail.com>
2025-05-17 19:11:37 +0200

    refactoring create-env-file.sh

--- docker/bin/create-env-file.sh
+++ docker/bin/create-env-file.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 R="\e[31m"
 G="\e[32m"
@@ -9,24 +10,22 @@ ENV_FILE=".env"
 ENV_FILE_TEMPLATE=".env.example"
 
 confirm() {
-    echo -e -n "${Y}${1:-Are yu sure?}${EC} [${R}y${EC}/${G}N${EC}] (continuing in 20s) "
-    read -t 20 -r -n 1 response
+    echo -e -n "${Y}${1:-Are you sure?}${EC} [${R}y${EC}/${G}N${EC}] (continuing in 20s) "
+    read -t 20 -r -n 1 response || true
     printf "\n"
-    if [[ "$response" =~ ^[yY]$ ]]; then
+    if [[ "${response:-}" =~ ^[yY]$ ]]; then
         return 0
     fi
     return 1
 }
 
 if [ ! -f "${ENV_FILE}" ]; then
-    cp ${ENV_FILE_TEMPLATE} ${ENV_FILE}
+    cp "${ENV_FILE_TEMPLATE}" "${ENV_FILE}"
     echo -e "${Y}.env${EC} file created from ${Y}.env.example${EC}"
 else
-    if
-      [[ 2 -eq $(md5sum "${ENV_FILE}" "${ENV_FILE_TEMPLATE}" | awk '{print $1}' | uniq | wc -l) ]] \
-      && confirm "The contents of the '${ENV_FILE}' file are different from the contents of '${ENV_FILE_TEMPLATE}'. Update the '${ENV_FILE}' file?";
-    then
-        cp ${ENV_FILE_TEMPLATE} ${ENV_FILE}
+    if ! cmp -s "${ENV_FILE}" "${ENV_FILE_TEMPLATE}" \
+      && confirm "The contents of '${ENV_FILE}' differ from '${ENV_FILE_TEMPLATE}'. Overwrite '${ENV_FILE}'?"; then
+        cp "${ENV_FILE_TEMPLATE}" "${ENV_FILE}"
         echo -e "The ${Y}${ENV_FILE}${EC} file has been updated."
     fi
 fi
